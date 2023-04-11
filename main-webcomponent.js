9382982bfd4c3ff85c703aa8b262a725a8c7eb73
FabianZosgornik <127842697+FabianZosgornik@users.noreply.github.com>
2023-04-11 09:25:14 +0200

    Update main-webcomponent.js

--- main-webcomponent.js
+++ main-webcomponent.js
@@ -6,27 +6,12 @@
         width: 100%;
       }
     </style>
-    <form id="form" autocomplete="off">
-        <fieldset> 
-          <legend>General</legend>
-          <table>
-            <tr>
-              <td><label for="REST API URL">REST API URL</label></td>
-              <td><input id="restapiurl" name="restapiurl" type="text"></td>
-            </tr>
-            <tr>
-              <td><label for="Widget Name">Widget Name</label></td>
-              <td><input id="name" name="name" type="text"></td>
-            </tr>
-          </table>
-        </fieldset>
-        <button type="submit" hidden>Submit</button>
-      </form>
-      <img id="image" src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" alt="">
+    <img id="image" src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" alt="">
     `;
   
   class ImageAPICall extends HTMLElement {
     constructor() {
+      super();
       this._export_settings = {};
       this._export_settings.apiurl = "";
       this._export_settings.imageurl = "";
@@ -34,13 +19,6 @@
       this._shadowRoot = this.attachShadow({ mode: "open" });
       this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
-      super();
-      this.init();
-    }
-
-    init() {
-      let shadowRoot = this.attachShadow({ mode: "open" });
-      shadowRoot.appendChild(tmpl.content.cloneNode(true));
       this.addEventListener("click", (event) => {
         var event = new Event("onClick");
         this.callAPI();
